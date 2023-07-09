24d527eea967d951add7298e5c26879f1d27ee36
BrunnoFardindeSouza <136929628+BrunnoFardindeSouza@users.noreply.github.com>
2023-07-09 10:34:06 -0300

    .

--- js/main.js
+++ js/main.js
@@ -52,3 +52,59 @@ btn_acessaradm[1].addEventListener('click',()=>{
 btn_voltaradm.addEventListener('click',()=>{
     paineladmintrador.style.display = 'none'
 })
+
+/* APLICAÇÕES DO MENU ADMINISTRATIVO */
+
+const select = document.getElementsByTagName('select')[0];
+const selecionar = document.getElementById('selecionar');
+const salvaralteracoes = document.getElementById('Salvar');
+selecionar.addEventListener("click",()=>{
+    const indice = select.selectedIndex
+    console.log(indice)
+    if(indice == 0){
+        AlterarLogo(' Link da imagem da logo')
+    }else if(indice == 1){
+            AlterarTitulo(' ex : BFinfromática')
+          
+    }else if(indice == 2){
+        AlterarTamanhoFonte(' ex: 16px') 
+
+    }else if(indice == 3){
+        AlterarCorFundocabeçalho(' (Cor do cabeçalho) ex: black')
+    }else if(indice == 4){
+        AlterarCorFundocorpo(' (cor do corpo ) ex: white')
+    }else if(indice == 5){
+        AlterarCorMousehover(' ex : aqua')             
+    }
+
+})
+function CriarInput(texto){
+    const estilizacao = document.getElementById('estlização')
+    const div = document.createElement('div')
+    const input = document.createElement('input')
+    div.setAttribute('id','divmod')
+    input.setAttribute('class','inputs')
+    input.placeholder=texto
+    div.appendChild(input)
+    estilizacao.appendChild(div)
+}
+function AlterarLogo(texto){
+    CriarInput(texto)
+}
+function AlterarTitulo(texto){
+    CriarInput(texto)
+}
+function AlterarTamanhoFonte(texto){
+    CriarInput(texto) 
+}
+function AlterarCorFundocabeçalho(texto){
+    CriarInput(texto) 
+}
+function AlterarCorFundocorpo(texto){
+    CriarInput(texto)   
+}
+function AlterarCorMousehover(texto){
+    CriarInput(texto)  
+}
+salvaralteracoes.addEventListener('click',()=>{
+})
