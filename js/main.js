d532e06baf91752a315bf90a118d5c78fd2eb313
BrunnoFardindeSouza <136929628+BrunnoFardindeSouza@users.noreply.github.com>
2023-07-12 12:42:50 -0300

    .

--- js/main.js
+++ js/main.js
@@ -69,16 +69,30 @@ const secao3 = templates.map((e,i,a)=>{
     return e.firstElementChild.nextElementSibling.nextElementSibling
 })
 
-function modificar01(el,link){
-        console.log(secao1[el].firstElementChild.src=link)
+function modificarsecao01(el,img){
+        secao1[el].firstElementChild.src=img
     
 }
-modificar01(0,'www')
+function modificarsecao02(el,texto){
+   secao2[el].firstElementChild.innerHTML=texto
+
+}
+function modificarsecao03(el,link){
+    console.log(secao3[el].firstElementChild.firstElementChild.setAttribute('href',link))
+
+}
 
 // Modificando templates
 
 
 /*Modificar 1 */
+modificarsecao01(0,'https://i6g8k8z4.stackpathcdn.com/wp-content/uploads/2021/07/turistas-em-buenos-aires.jpg')
+modificarsecao02(0,' Buenos Aires')
+modificarsecao03(0,'https://www.youtube.com/watch?v=Zs238FXKuTw')
 
 
 /*Modificar 2 */
+
+modificarsecao01(1,'https://s2-autoesporte.glbimg.com/7fbDefzwFWKIJSXO2J_ye_Xevlo=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/K/g/DhmSqxSuWzLeT3svOAzQ/2019-07-12-supraantig.jpg')
+modificarsecao02(1,'Supra')
+modificarsecao03(1,'https://www.youtube.com/watch?v=GQFIV3u-dTU')
